bb3e18f306caf985db147cb5fceadaa27ee5c569
Lan Ejiri <lan.ejiri@yahoo.com>
2018-05-06 20:19:21 -0400

    actaully scraping the right stuff now i think

--- server.js
+++ server.js
@@ -17,39 +17,52 @@ mongoose.connect("mongodb://localhost/articlescraperhwdb");
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
-    // First, we grab the body of the html with request
-    axios.get("http://www.kotaku.com/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-  
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("a.js_entry-link").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
-  
-        // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
-          .text();
-        result.link = $(this)
-          .attr("href");
-  
-        // Create a new Article using the `result` object built from scraping
-        db.article.create(result)
-          .then(function(dbArticle) {
-            // View the added result in the console
-            console.log(dbArticle);
-          })
-          .catch(function(err) {
-            // If an error occurred, send it to the client
-            return res.json(err);
-          });
-      });
-  
-      // If we were able to successfully scrape and save an Article, send a message to the client
-      res.send("Scrape Complete");
+  // First, we grab the body of the html with request
+  axios.get("http://www.kotaku.com/").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article header").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("h1").children("a.js_entry-link").text();
+      result.link = $(this).children("h1").children("a.js_entry-link").attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.article
+        .create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
     });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
   });
-  
-  app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
