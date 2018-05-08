25d0dfee05e2d7a0bdcfd70ada3d6f631b100370
Lan Ejiri <lan.ejiri@yahoo.com>
2018-05-07 20:34:09 -0400

    add remove from favorites works

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.set("view engine", "handlebars");
 app.get("/", function(req, res) {
   res.render("index");
 });
+//[[works, just shows navbar and scarpe button]]
 
 //route for scraping, just a button that re-directs to /articles
 app.get("/scrape", function(req, res) {
@@ -73,10 +74,10 @@ app.get("/scrape", function(req, res) {
     res.redirect("/articles");
   });
 });
+//[[works, figure out how to not scrape duplicates]]
 
 // show all articles in databse
 app.get("/articles", function(req, res) {
-
   db.article
     .find({})
     .then(function(dbArticle) {
@@ -86,6 +87,7 @@ app.get("/articles", function(req, res) {
       res.json(err);
     });
 });
+//[[works, shows all articles]]
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
@@ -95,7 +97,7 @@ app.get("/articles/:id", function(req, res) {
     // ..and populate all of the notes associated with it
     .populate("Comment")
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client \
+      // If we were able to successfully find an Article with the given id, send it back to the client
 
       res.render("comments", { obj: dbArticle });
     })
@@ -105,31 +107,42 @@ app.get("/articles/:id", function(req, res) {
     });
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  db.comments
-    .create(req.body)
-    .then(function(dbComment) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.article.findOneAndUpdate(
-        { _id: req.params.id },
-        { comments: dbComment._id },
-        { new: true }
-      );
-    })
+// shows all favorites
+app.get("/favorites", function(req, res) {
+  db.article
+    .find({ saved: "true" })
     .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
+      res.render("favorites", { article: dbArticle });
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
+app.get("/favorites/:id", function(req,res){
+  
+  db.article.findOneAndUpdate({ _id: req.params.id }, { $set: { saved: "true" } })
+    .then(function(dbArticle) {
+      res.redirect("/favorites");
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+})
+
+app.get("/unfavorite/:id", function(req,res){
+  
+  db.article.findOneAndUpdate({ _id: req.params.id }, { $set: { saved: "false" } })
+    .then(function(dbArticle) {
+      res.redirect("/favorites");
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+})
+
+
+// listening
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
